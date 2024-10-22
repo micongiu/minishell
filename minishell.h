abacec8637f59c6efc5b21067ec4e7ad9795dc25
Michele Congiu <micongiu@e3r9p1.42roma.it>
2024-10-22 14:12:49 +0200

    init

---
+++ minishell.h
@@ -0,0 +1,12 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include "ft_printf/ft_printf.h"
+# include "libft/libft.h"
+
+int	main(int argc, char **argv);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "../minishell.h"
+
+int	main(int argc, char **argv)
+{
+	ft_printf("ciao\n");
+}

3e76a12a9cfa7862d7cb43ea035c047e30efe41a
anmedyns <anmedyns@student.42roma.it>
2024-11-27 17:22:17 +0100

    update

--- src/ft_process.c
+++ src/ft_process.c
@@ -9,6 +9,7 @@ void	ft_add_process_node(t_process_list **process_list, t_process_list *new_var)
 		*process_list = new_var;
 	else
 	{
+
 		while (temp->next)
 			temp = temp->next;
 		temp->next = new_var;
@@ -34,37 +35,39 @@ t_process_list	*ft_create_process_node(char **tokens)
 	node->file_fd = NULL;
 	node->redirection = 0;
 	node->full_process = NULL;
-	node->argument = malloc(sizeof(char*) * i);
-
-	while(j < i)
+	node->argument = malloc(sizeof(char*) * i + 1);
+	while(j < i + 1)
 	{
 		node->argument[j] = NULL;
 		j++;
 	}
+	//node->argument[j] = NULL;
 	node->next = NULL;
 	return node;
 }
 
+//funzione di chat per printare la listonaaaaa
+
 void print_process_list(t_process_list *head) {
-	int i;
-    if (head) {
-		i = 0;
+    while (head != NULL) { // Itera finché ci sono nodi nella lista
         printf("Command: %s\n", head->command);
-        if (head->option) printf("Option: %s\n", head->option);
-        if (head->argument)
-		{
-			while(head->argument[i] != NULL)
-			{
-				printf("Argument %i: %s\n",i, head->argument[i]);
-				i++;
-			}
-		}
+        if (head->option) {
+            printf("Option: %s\n", head->option);
+        }
+
+        int i = 0;
+        while (head->argument != NULL && head->argument[i] != NULL) { // Controlla se 'argument' è valido
+            printf("Argument: %s\n", head->argument[i]);
+            i++;
+        }
+
         if (head->file_fd) {
-            printf("File FD: %s\n", head->file_fd);
+            printf("File FD: %s\n", head->file_fd); // Usa %d se file_fd è un int
             printf("Redirection: %d\n", head->redirection);
         }
+
         printf("----\n");
-        head = head->next;
+        head = head->next; // Passa al nodo successivo
     }
 }
 
@@ -81,9 +84,10 @@ t_process_list	*ft_init_process_list(char **tokens)
 	{
 		if (!current_node)
 		{
-			current_node = ft_create_process_node(tokens);//se non esiste un nodo , lo crea
+			current_node = ft_create_process_node(tokens);
+			arg = 0;
 		}
-		if (ft_strncmp(*tokens, "<", 2) == 0) //da qui
+		if (ft_strncmp(*tokens, "<", 2) == 0)
 		{
 			current_node->redirection = 1;
 			tokens++;
@@ -95,7 +99,7 @@ t_process_list	*ft_init_process_list(char **tokens)
 			tokens++;
 			current_node->file_fd = ft_strdup_lib(*tokens);
 		}
-		else if (ft_strncmp(*tokens, "<<", 3) == 0) // ci salviamo il delemitatore subito dopo le <<
+		else if (ft_strncmp(*tokens, "<<", 3) == 0)
 		{
 			current_node->redirection = 3;
 			tokens++;
@@ -106,10 +110,12 @@ t_process_list	*ft_init_process_list(char **tokens)
 			current_node->redirection = 4;
 			tokens++;
 			current_node->file_fd = ft_strdup_lib(*tokens);
-		}										//a qui controlla eventuali redirection e si salva il file in cui dovra essere scritto l'output o viceversa
+		}
 		else if (!current_node->command)
 		{
+			current_node->argument[arg] = ft_strdup_lib(*tokens);
 			current_node->command = ft_strdup_lib(*tokens);
+			arg++;
 		}
 		else if ((tokens != NULL) && (ft_strncmp(*tokens, ">", 2) != 0) && (ft_strncmp(*tokens, "<", 2) != 0) && (ft_strncmp(*tokens, ">>", 3) != 0) && (ft_strncmp(*tokens, "<<", 3) != 0) && (ft_strncmp(*tokens, "|", 2) != 0)) //SISTEMA DIO CARO
 		{
@@ -118,7 +124,6 @@ t_process_list	*ft_init_process_list(char **tokens)
 				current_node->option = ft_strdup_lib(*tokens);
 			}
 			current_node->argument[arg] = ft_strdup_lib(*tokens);
-			printf("argomento %i = %s\n", arg, current_node->argument[arg]);
 			arg++;
 		}
 		else if((ft_strncmp(*(tokens), "|", 2) != 0) && !*(tokens) && !(**tokens == '-'))
@@ -139,27 +144,25 @@ t_process_list	*ft_init_process_list(char **tokens)
 	return head;
 }
 
-void	free_process_list(t_process_list **cur)
-{
-	t_process_list	*tmp;
-	int j;
-	j = 0;
-	while (*cur != NULL)
-	{
+void free_process_list(t_process_list **cur) {
+	t_process_list *tmp;
+
+	while (*cur != NULL) {
 		tmp = (*cur)->next;
+
 		free((*cur)->command);
-		if ((*cur)->option)
+		if ((*cur)->option) {
 			free((*cur)->option);
-		if ((*cur)->file_fd)
+		}
+		if ((*cur)->file_fd) {
 			free((*cur)->file_fd);
-		if ((*cur)->full_process)
+		}
+		if ((*cur)->full_process) {
 			free((*cur)->full_process);
-		if ((*cur)->argument != NULL)
-		{
-			while ((*cur)->argument[j] != NULL)
-			{
+		}
+		if ((*cur)->argument) {
+			for (int j = 0; (*cur)->argument[j] != NULL; j++) {
 				free((*cur)->argument[j]);
-				j++;
 			}
 			free((*cur)->argument);
 		}
@@ -168,18 +171,6 @@ void	free_process_list(t_process_list **cur)
 	}
 }
 
-
-//Il simbolo > è utilizzato per redirigere l'output di un comando verso un file. Se il file non esiste, viene creato; se il file esiste già, il suo contenuto viene sovrascritto.
-//Il simbolo < è utilizzato per redirigere l'input da un file invece che dalla tastiera. Quando un comando si aspetta dell'input da tastiera, < permette di leggere i dati da un file invece che dall'utente.
-//Il simbolo << è utilizzato per creare un "here document", che permette di passare un blocco di testo direttamente a un comando. Questo è utile quando vuoi fornire più righe di input a un comando senza dover scrivere un file separato.
-//Il simbolo >> è simile a >, ma invece di sovrascrivere il contenuto del file, aggiunge (append) l'output alla fine del file esistente. Se il file non esiste, viene creato.
-
-
 //serve commentare e mettere a norma
-//aggiungere eventuale ERROR per troppi argomenti passati
 
-
-
-//COSE DA FARE
-//salva il full_path come array di stringhe    char[] = {"ciao","duce","ok",NULL};
 

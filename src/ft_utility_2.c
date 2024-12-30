da0d8998960abd21b56e524e07edcf7db365de90
Orobianco <giucon.mike@outlook.it>
2024-12-30 22:02:28 +0100

    norma ed ora si stampa il g_status, ma ci sono ancora cose da sistemare

--- src/ft_utility_2.c
+++ src/ft_utility_2.c
@@ -12,11 +12,92 @@
 
 #include "../minishell.h"
 
-void	close_and_update_fd(int *prev_fd, int *pipe_fd)
+extern int	g_status;
+
+int	ft_is_pipe_or_red(char c)
 {
-	if (*prev_fd != -1)
-		close(*prev_fd);
-	if (pipe_fd[1] != -1)
-		close(pipe_fd[1]);
-	*prev_fd = pipe_fd[0];
+	if (c != '\0' && !(c == '|' || c == '<' || c == '>'))
+		return (1);
+	return (0);
+}
+
+char	get_first_quote(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\'' || str[i] == '\"')
+			return (str[i]);
+		i++;
+	}
+	return (0);
+}
+
+char	*remove_quotes(char *str)
+{
+	int		i;
+	int		j;
+	char	*new_str;
+	char	quote_type;
+
+	i = 0;
+	j = 0;
+	if (!str)
+		return (NULL);
+	quote_type = get_first_quote(str);
+	new_str = ft_calloc((ft_strlen_lib(str) + 1), sizeof(char));
+	if (!new_str)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		if (str[i] != quote_type)
+			new_str[j++] = str[i];
+		i++;
+	}
+	new_str[j] = '\0';
+	return (new_str);
+}
+
+t_var_count	handle_dollar_question(char **line, char **token, t_var_count count)
+{
+	(*token)[count.j++] = **line;
+	(*line)++;
+	(*token)[count.j++] = **line;
+	(*line)++;
+	count.i += 2;
+	return (count);
+}
+
+t_var_count	ft_ex_dollar(char *line, char *token,
+	t_env_var *env, t_var_count count)
+{
+	int	j;
+	int	tmp;
+
+	while (line && *line != '$')
+		line++;
+	if (*line == '$' && (*(line + 1) == '?'))
+		return (count = handle_dollar_question(&line, &token, count));
+	line++;
+	while (env)
+	{
+		j = 0;
+		tmp = count.j;
+		if (ft_strncmp(line, env->name, ft_strlen_lib(env->name)) == 0)
+		{
+			while (env->value[j])
+				token[tmp++] = env->value[j++];
+			count.i += ft_strlen_lib(line) + 1;
+			count.j += tmp;
+			break ;
+		}
+		env = env->next;
+	}
+	if (env == NULL)
+		count.i++;
+	return (count);
 }
